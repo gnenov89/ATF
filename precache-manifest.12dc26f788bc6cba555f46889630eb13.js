fcf322ca837d271c146f85dadb7ef4e50217d992
gnenov89 <georgi.nenov89@gmail.com>
2020-01-17 15:25:16 -0500

    Updates

---
+++ precache-manifest.12dc26f788bc6cba555f46889630eb13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b9de692cf57218c4975b90d04eb803",
+    "url": "/ATF/index.html"
+  },
+  {
+    "revision": "b2fcd174f0cf962af967",
+    "url": "/ATF/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "cb22a3c1984ba23d40ca",
+    "url": "/ATF/static/js/2.d0e5ff81.chunk.js"
+  },
+  {
+    "revision": "e76927ded4e98c8bb3c1b6b2d3f786e8",
+    "url": "/ATF/static/js/2.d0e5ff81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2fcd174f0cf962af967",
+    "url": "/ATF/static/js/main.ca5a82c3.chunk.js"
+  },
+  {
+    "revision": "291af0e6eb425793db5a",
+    "url": "/ATF/static/js/runtime-main.90389b84.js"
+  }
+]);
